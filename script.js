2409350665be93934c2a5b3ba88512fa488538b0
roese025 <nateroese@Nathans-MacBook-Pro-3.local>
2017-05-02 21:57:18 -0500

    home

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	
+    $("#newsheader").mouseenter(function() {
+		$("#newsline").animate({ left: 300; });
+	});
+
+	$("#logo").mouseleave(function() {
+		$("#header").animate({ left: 0; });
+	});
+    
+    
+    
+});
